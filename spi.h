5a05bb8d66c6371c916af7e2db05814ffe5c1453
Narendran Kumaragurunathan <narenkn@gmail.com>
2014-05-08 16:35:36 +0530

    Added TFT driver and tests

--- spi.h
+++ spi.h
@@ -15,7 +15,6 @@
 #define SPI_SD             SPCR = 0x52
 #define SPI_HIGH_SPEED     SPCR = 0x50; SPSR |= (1<<SPI2X)
 
-
 void spi_init(void);
 uint8_t SPI_transmit(uint8_t);
 uint8_t SPI_receive(void);
