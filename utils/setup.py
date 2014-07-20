68069b91736ee7dac640f830f6d497f3fd55c741
Narendran Kumaragurunathan <narenkn@gmail.com>
2014-07-21 01:09:29 +0530

    checked creation of exe file

--- utils/setup.py
+++ utils/setup.py
@@ -1,4 +1,12 @@
 from distutils.core import setup
 import py2exe
+import sys
 
-setup(console=['prog.py'])
+prog = sys.argv.pop()
+sys.argv.append('py2exe')
+
+setup(
+  options={'py2exe': dict(bundle_files=1, optimize=2)},
+  console=[prog],
+  zipfile=None,
+  )
